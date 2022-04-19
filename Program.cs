77815ab4cb63c93e45fd89a0ef12f9bfb996131c
Nadir <nadir.t.1991@gmail.com>
2022-04-19 23:04:08 +0300

    FirstHomeWork is done

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+
+//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Input First Integer number: ");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Second Integer number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numa > numb) 
+{
+    Console.WriteLine("Большое число = {num a} ");
+    Console.WriteLine("Меньшее число = {num b} ");
+}
+
+else 
+{
+    Console.WriteLine("Большое число = {num b}");
+    Console.WriteLine("Меньшее число = {num a}");
+}
+
+*/
+
+/*
+
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input First Integer number: ");
+int number1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Second Integer number: ");
+int number2= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Thirst Integer number: ");
+int number3= Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number1>max)max=number1;
+if (number2>max)max=number2;
+if (number3>max)max=number3;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
+*/
+
+/*
+
+//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Write("Input Integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Number is Четное число");
+}
+
+else
+{
+    Console.WriteLine("Number is Нечетное число");
+}
+
+*/
+
+/*
+
+//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Input Integer n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+int a =1;
+int a1 = a+1;
+
+for (a = 1;a1 <= n; a1 = a + 1);
+
+if (a1%2==0)
+    {
+        Console.WriteLine(" {a1} ,  ");
+    }
+Console.WriteLine("Все четные числа от 1 до {n}: ");
+
+*/
